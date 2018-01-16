e163bdc2a999b10dee1248b3870454ca6771c917
yinhang <yinhang@dehuinet.com>
2018-01-16 10:23:37 +0800

    proxy

--- config/index.js
+++ config/index.js
@@ -14,6 +14,7 @@ module.exports = {
         '/api/*': {
             target: 'http://localhost:3030',
             secure: false,
+            changeOrigin: true,
             pathRewrite: {
               '^/api': ''
             }
@@ -21,7 +22,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -31,7 +32,7 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
+    useEslint: false,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
